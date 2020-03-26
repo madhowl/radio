69af6ffc693c2c5e98e93fbff167a6a057aac10a
Disssly <razigraevvlad@gmail.com>
2020-03-26 11:27:22 +1000

    edit som tpl

--- resources/views/genres/list.blade.php
+++ resources/views/genres/list.blade.php
@@ -4,28 +4,38 @@
 
 @section('content')
     <div class="row">
-        <table>
-            <th>Название жанра</th>
+      <table>
+          <th>Название жанра</th>
 
-            @foreach( $categories as $category)
-                <tr>
-                <td><a href="/genres/{{$category->id}}">{{$category->title}}</a></td>
-            </tr>
-            @endforeach
-        </table>
-        <div class="col-sm-2">
-            <!-- Card -->
-            <div class="card navcolor navtext">
+          @foreach( $categories as $category)
+              <tr>
+              <td><a href="/genres/{{$category->id}}">{{$category->title}}</a></td>
+          </tr>
+          @endforeach
+      </table>
+      <div class="col-sm-2">
+      <!-- Card -->
+<div class="card">
 
-                <!-- Card image -->
-                <div class="view overlay">
-                    <img class="card-img-top wow fadeInUp" data-wow-delay="0.6s" src="https://bipbap.ru/wp-content/uploads/2018/06/The-catalyst-single-cover-500x500.png" alt="Card image cap">
-                    <a href="#!" data-toggle="modal" data-target="#exampleModalCenter">
-                        <div class="mask rgba-white-slight"></div>
-                    </a>
-                </div>
-            </div>
-        </div>
+  <!-- Card image -->
+  <div class="view overlay">
+    <img class="card-img-top wow fadeInUp" src="https://bipbap.ru/wp-content/uploads/2018/06/The-catalyst-single-cover-500x500.png"
+      alt="Card image cap">
+    <a href="/1">
+      <div class="mask rgba-white-slight"></div>
+    </a>
+  </div>
+
+  <!-- Card content -->
+  <div class="card-body">
+
+    <!-- Title -->
+    <h4 class="card-title">Card Title</h4>
+
+  </div>
+
+</div>
+</div>
+<!-- Card -->
     </div>
-    <!-- Modal -->
 @endsection
